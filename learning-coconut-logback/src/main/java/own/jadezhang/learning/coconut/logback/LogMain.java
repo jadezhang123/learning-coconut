f4c62d18af959ae8d7209a2d1c23e35a870c563b
zhangjunwei <604110740@qq.com>
2017-04-21 11:30:18 +0800

    添加log4j2；设置logback filter

--- learning-coconut-logback/src/main/java/own/jadezhang/learning/coconut/logback/LogMain.java
+++ learning-coconut-logback/src/main/java/own/jadezhang/learning/coconut/logback/LogMain.java
@@ -10,12 +10,12 @@ public class LogMain {
     private final static Logger logger = LoggerFactory.getLogger(LogMain.class);
 
     public static void main(String[] args) {
-        logger.debug("this is test for log4j12[debug]");
-        logger.info("this is test for log4j12[info]");
+        logger.debug("this is test for logback[debug]");
+        logger.info("this is test for logback[info]");
         try {
             int a = 5/0;
         }catch (Exception e){
-            logger.debug("method[%s] in %s throw a exception : %s", "main", "LogMain", e.getMessage());
+            logger.debug("method[{}] in {} throw a exception : {}", "main", "LogMain", e.getMessage());
             logger.error("throw a exception", e);
         }
     }
---
+++ learning-coconut-springLog4j2/src/main/java/own/jadezhang/learning/log4j2/LogMain.java
@@ -0,0 +1,29 @@
+package own.jadezhang.learning.log4j2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Zhang Junwei on 2017/4/20 0020.
+ */
+@Component
+public class LogMain {
+    
+    private final static Logger logger = LoggerFactory.getLogger(LogMain.class);
+
+    public void logMsg(String msg){
+        logger.debug("this is a test for log4j2 with debug msg:" + msg);
+        logger.info("this is a test for log4j2 with info msg:" + msg);
+    }
+
+
+    public static void main(String[] args) {
+        ApplicationContext xmlACtx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        LogMain bean = xmlACtx.getBean(LogMain.class);
+        bean.logMsg("jack");
+    }
+
+}
